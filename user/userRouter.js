3fcdfb21d4d844b1777dd1cc41bea54f91b2f9a1
Eli Sacks <bastlifa@gmail.com>
2019-09-23 02:59:39 -0400

    added router.use cors to routers

--- user/userRouter.js
+++ user/userRouter.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const Users = require('./authModel')
 const restricted = require('../restricted-middleware/restricted')
+const cors = require('cors')
 
+router.use(cors())
 
 router.get('/', restricted, (req, res) =>
 {
