3739024cfa42fffb4bd68699f4a30068caffbc94
Eli Sacks <bastlifa@gmail.com>
2019-09-23 01:54:51 -0400

    beggings of swagger documentation

--- api/server.js
+++ api/server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger.json')
 const authRouter = require('../auth/authRouter')
-
 
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 server.use('/api/auth', authRouter)
 
 
